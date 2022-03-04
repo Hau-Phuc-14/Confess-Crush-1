4f5175b3d445f909e8980f38d6bf7508b0e94a27
Hau-Phuc-14 <100937534+Hau-Phuc-14@users.noreply.github.com>
2022-03-04 19:30:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "nhớ thành thật vào nghe chưa",
     introTitle: 'Tên người yêu bạn',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    mà s lại gặp zịt biết nói`,
+    btnIntro: '^^Nói đúng z^^',
+    title: 'Câu hỏi chân thành này ',
+    desc: 'Vy có "nu nốt" k mà mấy cái đơn giản thế cũng k biết',
+    btnYes: 'nói đúng thế🥰',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Từ nay chơi game k đc cáu bẩn nghe chưa',
+    btnReply: 'ok nhuôn😘',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'hỏi v chứ biết vy cáu bẩn bẩm sinh rồi =))',
+    messDesc: 'Tối nay 7h40, học bài nha h.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
